f3b3c2a50c19ac294c0149df8e854ca3207ad471
Илья <is@inpool.ru>
2020-12-12 20:18:32 +0300

    Render

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React from "react"
+import { Column } from "./Column"
+import { Card } from "./Card"
 import { AppContainer } from "./styles"
 
-function App() {
+const App = () => {
   return (
     <AppContainer>
-      Columns will go here
+      <Column text="To Do">
+        <Card text="Generate app scaffold" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn Typescript" />
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to use static typing" />
+      </Column>
     </AppContainer>
-  );
+  )
 }
 
-export default App;
+export default App
