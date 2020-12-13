81b5069db525d9465f0e3042d4f177e33c0476a4
Илья <is@inpool.ru>
2020-12-13 20:41:06 +0300

    Inout focus hook

--- src/NewItemForm.tsx
+++ src/NewItemForm.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react"
 import { NewItemFormContainer, NewItemButton, NewItemInput } from "./styles"
+import { useFocus } from "./utils/useFocus"
+
 
 interface NewItemFormProps {
     onAdd(text: string): void
@@ -7,11 +9,22 @@ interface NewItemFormProps {
 
 export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
     const [text, setText] = useState("")
+    const inputRef = useFocus()
+    const handleAddText = (
+        event: React.KeyboardEvent<HTMLInputElement>
+        ) => {
+        if (event.key === "Enter") {
+        onAdd(text)
+        }
+        }
+
     return (
         <NewItemFormContainer>
             <NewItemInput
                 value={text}
                 onChange={e => setText(e.target.value)}
+                ref={inputRef}
+                onKeyPress={handleAddText}
             />
             <NewItemButton onClick={() => onAdd(text)}>
                 Create
