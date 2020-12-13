c79c1715b46152cb83501888734d8583b611f2ad
Илья <is@inpool.ru>
2020-12-13 21:07:08 +0300

    Add state managment

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { AppStateProvider } from "./AppStateContext"
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <AppStateProvider>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </AppStateProvider>,
+  document.getElementById("root")
+)
